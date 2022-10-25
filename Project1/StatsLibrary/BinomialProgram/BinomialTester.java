1ef307ccb4e7be29a5c9bd6bb1d7037f2e38376e
codfsh9 <91903265+codfsh9@users.noreply.github.com>
2022-10-25 19:47:22 -0400

    Add files via upload

---
+++ Project1/StatsLibrary/BinomialProgram/BinomialTester.java
@@ -0,0 +1,11 @@
+public class BinomialTester {
+    //Main method
+    public static void main(String[] args)
+    {
+        //Create object of Binomial class
+        Binomial b = new Binomial(2, 3, .25, .25);
+        //Call method and return answer
+        b.calcBinomial();
+    }
+
+}
